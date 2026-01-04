c49f6baf7cc31dc33a44a8189732bdc77818f7c7
Roman Plis <plisroman@gmail.com>
2026-01-04 22:25:50 +0500

    feat: add new solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -100,8 +100,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
 }
 
 /**
@@ -115,8 +116,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+
+  const delta = end - start;
+  const deltaDays = Math.round(delta / (24 * 60 * 60 * 1000) + 1);
+  return deltaDays;
 }
 
 /**

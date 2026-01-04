2d3dde118e671a85d607451d48f1380eb98e86fb
Roman Plis <plisroman@gmail.com>
2026-01-04 22:10:12 +0500

    feat: add new solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -73,8 +73,20 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date.getTime());
+  const todayDay = result.getUTCDay();
+  const delta = 5 - todayDay;
+
+  let daysToAdd;
+  if (delta > 0) {
+    daysToAdd = delta;
+  } else {
+    daysToAdd = 7 + delta;
+  }
+
+  result.setUTCDate(result.getUTCDate() + daysToAdd);
+  return result;
 }
 
 /**
